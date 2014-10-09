e6262553d3fa0b0a1c4e560a04d14100fa687f46
Sean Yip <bouowmx@gmail.com>
2014-10-09 13:17:19 -0400

    Add map A-Z and a-z to [0-25]

--- tunez.c
+++ tunez.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int ord(char c) { //This is like Python's ord, which takes a character and returns its ASCII value. Here, it maps A-Z and a-z to [0-25].
+	if ((c >= 'A') && (c <= 'Z')) {return c - 'A';}
+	else if ((c >= 'a') && (c <= 'z')) {return c - 'a';}
+	else {return -1;}
+}
+
 int main() {
+	linked_list * music_library[26]
 	return EXIT_SUCCESS;
 }
 
