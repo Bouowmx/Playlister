7be56746c52e9c2a765b9887b7468630fca7dd20
Sean Yip <bouowmx@gmail.com>
2014-10-10 22:04:45 -0400

    Add music library operations

--- tunez.c
+++ tunez.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct music_library {linked_list * library[26];} music_library;
+
+music_library * music_library_new() {
+	linked_list * library_list[26];
+	unsigned char i;
+	for (i = 0; i < 26; i++) {library_list[i] = linked_list_new();}
+	music_library * library = (music_library *) malloc(sizeof(music_library));
+	library->library = library_list;
+	return library;
+}
+
 unsigned char character(unsigned char i) {return i + 'A';}
 
 char ord(char c) { //This is like Python's ord, which takes a character and returns its ASCII value. Here, it maps A-Z and a-z to [0-25].
@@ -11,9 +22,7 @@ char ord(char c) { //This is like Python's ord, which takes a character and retu
 }
 
 int main() {
-	linked_list * music_library[26];
-	unsigned int i;
-	for (i = 0; i < 26; i++) {music_library[i] = linked_list_new();}
+	music_library * library = music_library_new();
 	for (i = 0; i < 26; i++) {
 		printf("%c: ", character(i));
 		print_list(music_library[i]);
